0ea016943e9bc75be252c40011eae007eacd2983
aknathweb <aniknwu.cse@gmail.com>
2022-11-08 16:18:16 +0600

    done footer part

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import logo from './../../Shared/logo.png'
 const Footer = () => {
     return (
         <div>
-
+            <footer className="footer footer-center p-10 bg-gray-300 text-base-content rounded">
+                <div>
+                    <img src={logo} alt="" width={70} />
+                    <p className="font-bold">
+                        PrettyClick <br />Providing outstanding photography experience
+                    </p>
+                    <p>Copyright © 2022 - All right reserved</p>
+                </div>
+            </footer>
         </div>
     );
 };

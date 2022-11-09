f0e749e749de1fe2e0d2cb3c0193fac3e3717409
aknathweb <aniknwu.cse@gmail.com>
2022-11-10 00:26:58 +0600

    done unknown path problem

---
+++ src/Pages/Unknownroute/Unknownroute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Unknownroute = () => {
+    return (
+        <div>
+            <h1 className='text-red-700 font-extrabold text-2xl'>This Route Not Available</h1>
+        </div>
+    );
+};
+
+export default Unknownroute;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,9 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Login from "../Login/Login";
 import SignupWithEmail from "../Login/SignupWithEmail";
+import Blogs from "../Pages/Blogs/Blogs";
 import Home from "../Pages/Home/Home";
 import ServiceDetails from "../Pages/Services/ServiceDetails";
 import Services from "../Pages/Services/Services";
+import Unknownroute from "../Pages/Unknownroute/Unknownroute";
 import PrivateRoutes from "./PrivateRoutes";
 
 export const routes = createBrowserRouter([
@@ -32,6 +34,14 @@ export const routes = createBrowserRouter([
             {
                 path: '/signup',
                 element: <SignupWithEmail></SignupWithEmail>
+            },
+            {
+                path: '/blogs',
+                element: <Blogs></Blogs>
+            },
+            {
+                path: '*',
+                element: <Unknownroute></Unknownroute>
             }
         ])
     }
